4624be9fc4a536aabc91d69c9eeff6dc0cb10231
VeraDev <veradev0@proton.me>
2025-08-24 18:58:48 +0200

    Update HelloWorld.lua

--- Examples/HelloWorld.lua
+++ Examples/HelloWorld.lua
@@ -11,6 +11,7 @@ titleTextBox:setProperty("Caption", "Welcome to sm.regui!")
 titleTextBox:setProperty("FontName", "SM_HeaderLarge_Narrow")
 titleTextBox:setProperty("TextAlign", "Center")
 titleTextBox:setProperty("TextShadow", true)
+
 local text = "If you are able to see this ReGuiInterface, that means sm.regui should be working perfectly fine! You can now start using sm.regui's awesome features.\n" ..
              "\n" ..
              "All documentations are in the definition file"
@@ -23,4 +24,5 @@ descriptionEditbox:setProperty("Caption", text)
 descriptionEditbox:setProperty("ReadOnly", true)
 descriptionEditbox:setProperty("MultiLine", true)
 descriptionEditbox:setProperty("WordWrap", true)
+
 gui:open()
